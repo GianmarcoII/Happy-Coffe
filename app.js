ac08c618d3ecc6c779fb82adcc1a42c0e0ca1cc9
GianmarcoII <holyspeed@gmx.com>
2021-08-31 08:58:39 +0200

    Fixed Overflow caused by GSAP

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 $(function(){
-  alert("This is a showcase website: not all functionalities are available");
   let menu = document.querySelector('.menu');
   let hamburgerMenu = document.querySelector('.mobile');
   powerMobileMenu(menu,hamburgerMenu);
@@ -30,13 +29,13 @@ const animation = function(){
       scrollTrigger: "img",
       duration: 1,
       opacity: 1,
-      x: -10
+      y: -10
     });
     gsap.from(".banner_body",{
       scrollTrigger: ".banner_body",
       duration: 1,
       opacity: 1,
-      x: 20
+      y: 20
     });
   }catch(e){
     console.error(e);
